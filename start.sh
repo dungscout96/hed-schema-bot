5c6fd2e2366f5258de74d4d2d2e8dd2754efb3bf
Dung Truong <dt.young112@gmail.com>
2025-06-16 20:58:08 -0400

    testing

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Download config from S3
+
+echo "AWS_ACCESS_KEY_ID is: $AWS_ACCESS_KEY_ID"
+echo "AWS_SECRET_ACCESS_KEY is: $AWS_SECRET_ACCESS_KEY"
+echo "AWS_DEFAULT_REGION is: $AWS_DEFAULT_REGION"
+
 echo "Downloading litellm_config.yaml from S3..."
 aws s3 cp s3://litellmproxy/litellm_config.yaml /app/config.yaml --region us-east-2
 
